ab5b59b4ef10d80ab0134b6f36da5ff32ce4bb5c
Abdul Rehman <likeam99@gmail.com>
2024-06-02 22:47:43 +0500

    Building the delete section

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,45 +1,62 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import Button from 'react-bootstrap/Button';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Button from "react-bootstrap/Button";
+import { Link, useNavigate } from "react-router-dom";
+import { deleteUser } from "./UserReducer";
 
 const Home = () => {
+  const users = useSelector((state) => state.users);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const users = useSelector((state) => state.users)
-    console.log(users)
+  const handleDelete = (id) => {
+    dispatch(
+      deleteUser({
+        id: id,
+      })
+    );
 
-    return (
-        <div className="container">
-            <h2>Crud App with JSON Server   </h2>
-            <Link to="/create"><Button className="btn btn-sucess my-3">Create +</Button></Link>
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        users.map((user, i) => (
-                            <tr key={i}>
-                                <td>{user.id}</td>
-                                <td>{user.name}</td>
-                                <td>{user.email}</td>
-                                <td>
-                                    <Link to={`/update/${user.id}`}><Button className="btn btn-sucess mx-1">Edit</Button></Link>
-                                    <Link to={`/update/${user.id}`}><Button className="btn btn-danger mx-1">Delete</Button></Link>                                   
-                                </td>
-                            </tr>
+    navigate("/");
+  };
 
-                        ))
-                    }
-                </tbody>
-            </table>
-        </div>
-    )
-}
+  return (
+    <div className="container">
+      <h2>Crud App with JSON Server </h2>
+      <Link to="/create">
+        <Button className="btn btn-sucess my-3">Create +</Button>
+      </Link>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, i) => (
+            <tr key={i}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>
+                <Link to={`/update/${user.id}`}>
+                  <Button className="btn btn-sucess mx-1">Edit</Button>
+                </Link>
+                <Button
+                  onClick={() => handleDelete(user.id)}
+                  className="btn btn-danger mx-1"
+                >
+                  Delete
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
